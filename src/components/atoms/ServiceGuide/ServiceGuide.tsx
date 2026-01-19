49995f813573a7d86b63d40621da86c573a0cb35
victor <ejioguvictor6@gmail.com>
2026-01-19 08:15:23 +0100

    updated: service section

---
+++ src/components/atoms/ServiceGuide/ServiceGuide.tsx
@@ -0,0 +1,15 @@
+const ServiceGuide = () => {
+  return (
+    <div className="w-full flex justify-center items-center flex-col text-[#3A3A3A] max-sm:text-sm">
+      <p className="text-center">Prices are starting points.</p>
+      <p className="text-center">
+        Final scope, timelines, and pricing may vary depending on the project.
+      </p>
+      <p className="text-center">
+        I’m always open to thoughtful conversations and flexible collaborations.
+      </p>
+    </div>
+  );
+};
+
+export default ServiceGuide;
